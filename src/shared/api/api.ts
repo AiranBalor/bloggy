28d5f088b6f9773738856988010cc291bbe4cfd4
Ilya Korotkov <airanvalor@mail.ru>
2022-11-30 15:49:38 +0300

    fix/ fixed remote server auth bug

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,15 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-  },
+});
+
+// при первичном открытии сайта на хостинге падает проблема с профилем
+// после авторизации в локальном хранилище пустая строка вместо переменной
+// добавил перехватчик, чтобы при любом запросе с заголовками эта переменная
+// принудительно еще раз вытаскивалась из хранилища.
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+  }
+  return config;
 });
