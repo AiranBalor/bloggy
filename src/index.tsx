4c176aaae9351028adc1cb7a81d2de971ec770d4
Ilya Korotkov <airanvalor@mail.ru>
2022-12-28 13:03:23 +0300

    dev/migrate from React 17 to 18, errors fixing

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { StoreProvider } from 'app/providers/StoreProvider';
@@ -9,7 +10,9 @@ import { ErrorBoundary } from './app/providers/ErrorBoundary';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-render(
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <BrowserRouter>
     <StoreProvider>
       <ErrorBoundary>
@@ -19,5 +22,4 @@ render(
       </ErrorBoundary>
     </StoreProvider>
   </BrowserRouter>,
-  document.getElementById('root'),
 );
